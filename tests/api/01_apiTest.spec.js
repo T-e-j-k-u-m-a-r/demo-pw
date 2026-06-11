5525241d43e4e9fffa4f14ad1b022fbed54115a9
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2026-06-11 12:25:58 +0530

    Feat: Added GET call script

---
+++ tests/api/01_apiTest.spec.js
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+
+test.only(`GET Call`, async ({request}) => {
+
+    const response = await request.get(`https://gorest.co.in/public/v2/users`)
+    const responseBody = await response.json();
+
+    // console.log(responseBody)
+    expect(responseBody.length).toBe(10)
+
+    const firstObj = responseBody[0];
+
+    console.log(`id = `+firstObj.id)
+    console.log(`name = `+firstObj.name)
+    console.log(`email = `+firstObj.email)
+    console.log(`gender = `+firstObj.gender)
+    console.log(`status = `+firstObj.status)
+
+    // Assertions
+    expect(await response.status()).toBe(200)
+    expect(await response.statusText()).toBe(`OK`)
+
+})
+
+test(`POST Call`, async ({request}) => {
+
+
+})
+
+test(`PUT Call`, async ({request}) => {
+
+
+})
+
+
+test(`DELETE Call`, async ({request}) => {
+
+
+})
